3c1287282b220360a2ee9e125a174e34a3940e91
Alex <alekceytr@mail.ru>
2019-01-12 23:30:09 +0800

    jump

--- main.py
+++ main.py
@@ -5,34 +5,50 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Game")
 
 x = 50
-y = 50
+y = 425
 width = 40
 height = 60
 speed = 5
 
+isJump = False
+jumCount = 10
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > 5:
         x -= speed
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - speed :
         x += speed
 
-    if keys[pygame.K_UP]:
-        y -= speed
+    if not(isJump):
+        if keys[pygame.K_UP] and y > 5:
+            y -= speed
 
-    if keys[pygame.K_DOWN]:
-        y += speed
+        if keys[pygame.K_DOWN] and y < 500 - height - speed:
+            y += speed
 
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumCount >= -10:
+            if jumCount < 0:
+                y += (jumCount ** 2) / 2
+            else:
+                y -= (jumCount ** 2) / 2
+            jumCount -= 1
+        else:
+            isJump = False
+            jumCount = 10
 
-
+    win.fill((0,0,0))
     pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
     pygame.display.update()
 
